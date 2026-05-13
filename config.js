62ecad015d14355f4821d9511b54147f01128a54
operatorsdittihamah <operatorsdittihamah@gmail.com>
2026-05-13 15:06:45 +0700

    ddsf

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbw_3TuT9866gd992lW8kWudvo9F1A67sYRAw0L-nV8RESQZbNfa4Ho7aS1vdUTmFVj8/exec";
+"https://script.google.com/macros/s/AKfycbw_GkSqcm66nZuM-NuNLm0_FeQJKkQpi-qCNVGZoi8iehiK43ObML4Ijo_pLTaqCBHy/exec";
