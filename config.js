5e63263734d768bc22801dcf1531c0f5b46b149c
operatorsdittihamah <operatorsdittihamah@gmail.com>
2026-05-13 14:11:31 +0700

    KGHKJGKKHHG

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbwqS40WwkzOpJ4gtOXc0_M71LeQH0-Ldkpdk3H9JiCD5lW92ICAN-_XzhjWxfa2r-CX/exec";
+"https://script.google.com/macros/s/AKfycbzMzMf5BxlI-zDRc5agtm9yNz1xJ7gcGsn4kZnEZ_y0-UBQgg_ABldqyofuXt0eVPnb/exec";
